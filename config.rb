9424034ac3474e4fff783f8cbf2d7f8d2119cbc5
Luqmaan <ldawoodjee@gmail.com>
2013-06-24 12:46:01 -0400

    Added ZURB

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
+require 'zurb-foundation'
 # Require any additional compass plugins here.
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -11,7 +13,7 @@ javascripts_dir = "js"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
@@ -19,6 +21,6 @@ javascripts_dir = "js"
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
---
+++ index.md
@@ -0,0 +1,24 @@
+---
+layout: default
+title: Luqmaan Dawoodjee (createch)
+---
+
+Javascript, Python, & iOS Developer
+--
+
+(Stuff I Build)[/portfolio]
+(Resume)[/resume]
+
+Web
+==
+- Fashion Feed
+- HARTxt
+- PyChecker
+- Goodbye Quora Embed Button
+- AVI-SPL
+
+Mobile
+==
+- Reeves Kiosk
+- Live Fresh For Less
+- CRVPanoramaImagePicker
