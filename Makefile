5c0826df465d53c5a9a90b1644a1598a7fe5a1ae
luqmaan <ldawoodjee@gmail.com>
2025-09-13 17:29:18 -0700

    meowdy

--- Makefile
+++
@@ -1,9 +0,0 @@
-start: stop serve
-	compass watch 
-
-serve:
-	nohup python -m SimpleHTTPServer 12345 &
-
-stop:
-	python stop.py
-	rm -f nohup.out
--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    if (Modernizr.touch) {
-        $.each($('.section'), function(i, item) {
-            $(item).addClass("touch")
-        })
-    }
-})
